9042892bb453bc84c742888183bf9801fc34d77a
Thomas L'Esperance <tmles2023@gmail.com>
2025-03-19 16:11:37 -0500

    Added more docstrings and type hinting

--- data_integration_elements/(DIE_template)/main.py
+++ data_integration_elements/(DIE_template)/main.py
@@ -13,13 +13,34 @@ from utils.die import DIE
 
 # --- For jobs that send a CSV file in an email:
 # def message_builder(job_config: Dict[str, Any], file_path: str) -> Tuple[str, str]:
-#     """Builds email subject and body for <job_name>."""
+#     """
+#     Assembles the custom email associated with job.
+
+#     Args:
+#         job_config: The combined, nested job configuration dictionary.
+#         file_path: The path to the attachment of the email, in case dynamic text
+#                     based on it is needed.
+
+#     Returns:
+#         A tuple containing the customized email subect and body text.
+#     """
 #     subject = "Example Email for <job_name> Integration"
 #     body = "This is a test email <job_name> job, which sends an email."
 #     return subject, body
 
 # --- For jobs that modify the CSV file or have more advanced procedures:
-# def custom_transform(header: List[str], data:  List[Tuple[Any,...]], intermediate_file_path):
+# def custom_transform(header: List[str], data:  List[Tuple[Any,...]], intermediate_file_path) -> str:
+#     """
+#     Performs the customized data transformation(s) unique to this DIE.
+
+#     Args:
+#         header: The first row of fields containing the untransformed data headers.
+#         data: The rows of untransformed data.
+#         intermediate_file_path: The path where the transformed data should be stored before loading.
+
+#     Returns:
+#         The file path to the completed and transformed data, ready to be loaded.
+#     """
 #     intermediate_file = export_csv_from_tuple_array(data, intermediate_file_path, header)
 #     # ... apply transformations to intermediate_file and store in transformed_file
 #     return transformed_file
@@ -29,7 +50,11 @@ message_builder = None
 custom_transform = None
 
 
-def main():
+def main() -> None:
+    """
+    Identifies the name of integration job from parent DIE dir name, instantiates DIE class,
+    passes custom funcitons to the class and runs the DIE object.
+    """
     job_name = Path(__file__).resolve().parent.name.replace("DIE_", "")
     die = DIE(
         job_name=job_name,
--- data_integration_elements/create_die.py
+++ data_integration_elements/create_die.py
@@ -3,9 +3,18 @@ import shutil
 import argparse
 
 
-def create_new_die(job_name: str, template_dir: Path, destination_dir: Path):
-    """Creates a new DIE directory based on the template files."""
+def create_new_die(job_name: str, template_dir: Path, destination_dir: Path) -> None:
+    """
+    Creates a new DIE directory based on the template files.
 
+    Args:
+        job_name: The name of the new DIE (without the 'DIE_' prefix on the job directory).
+        template_dir: The path to the directory containing the DIE template files (i.e., main.py and query.sql).
+        destination_dir: The parent directory of the new DIE directory.
+
+    Raises:
+        OSError: If shutil could not copy template directory tree to specified location.
+    """
     new_die_path = destination_dir / f"DIE_{job_name}"
 
     if new_die_path.exists():
@@ -27,6 +36,10 @@ def create_new_die(job_name: str, template_dir: Path, destination_dir: Path):
 
 
 def main():
+    """
+    Parses arguments passed to this script, and calls the helper function with the
+    provided command line arg.
+    """
     parser = argparse.ArgumentParser(
         description="Create a new Data Integration Element (DIE)."
     )
